efa2acaf626c482167f1c67d5c3eda5e1f32d27d
Abdurrahman Harits Ghiffary <129573632+abdurrahmanharitsghiffary@users.noreply.github.com>
2024-11-25 12:50:49 +0700

    chore: remove env value on .env

--- src/app/dashboard/employee/page.tsx
+++ src/app/dashboard/employee/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import type { SearchParams } from 'nuqs/parsers';
 import React, { Suspense } from 'react';
 
 import EmployeePageHeading from '@/app/dashboard/employee/_components/employee-page-heading';
@@ -8,7 +7,6 @@ import EmployeePageErrorBoundary from '@/app/dashboard/employee/_components/layo
 import { Separator } from '@/components/ui/separator';
 import DataTableSkeleton from '@/components/ui/table/data-table-skeleton';
 import EmployeeTableAction from '@/features/employee/components/employee-tables/employee-table-action';
-import { searchParamsCache, serialize } from '@/lib/searchparams';
 
 import EmployeeListing from './_components/employee-listing';
 
@@ -16,16 +14,7 @@ export const metadata: Metadata = {
   title: 'Dashboard : Employees',
 };
 
-interface PageProps {
-  searchParams: SearchParams;
-}
-
-export default async function Page({ searchParams }: PageProps) {
-  searchParamsCache.parse(searchParams);
-
-  // This key is used for invoke suspense if any of the search params changed (used for filters).
-  const key = serialize({ ...searchParams });
-
+export default async function Page() {
   return (
     <EmployeePageContainer>
       <EmployeePageHeading />
@@ -33,7 +22,7 @@ export default async function Page({ searchParams }: PageProps) {
       <div className="space-y-4">
         <EmployeeTableAction />
         <EmployeePageErrorBoundary>
-          <Suspense key={key} fallback={<DataTableSkeleton />}>
+          <Suspense fallback={<DataTableSkeleton />}>
             <EmployeeListing />
           </Suspense>
         </EmployeePageErrorBoundary>
